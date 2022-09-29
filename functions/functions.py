3a297dc3ebfa639e64de630e27a1fc1f3e1da2ea
sa1n255 <samuellaurindodelima2004@gmail.com>
2022-09-29 14:28:22 -0300

    pequenas melhorias

--- functions/functions.py
+++ functions/functions.py
@@ -1,11 +1,35 @@
 from functions.colors import *
+from platform import python_version_tuple
 from string import ascii_letters as all_letters, digits as all_numbers
 from secrets import choice
 from itertools import count
 from functions import exceptions
 
-# define oque * irá importar
-__all__ = ['show_banner', 'password_generator']
+import sys
+
+# define oque * irá importar desse arquivo
+__all__ = ['show_banner', 'password_generator', 'version_check', 'confirm']
+
+
+def version_check():
+    python_version_major = int(python_version_tuple()[0])
+    python_version_minor = int(python_version_tuple()[1])
+
+    if python_version_major == 3:
+        if python_version_minor < 9:
+            print(vermelho + "Python 3.9 ou acima" + reset)
+            print(vermelho + "Encerrando..." + reset)
+            sys.exit(1)
+        else:
+            pass
+
+    elif python_version_major > 3: 
+        pass
+
+    else:
+        print(vermelho + "Python 3.9 ou acima" + reset)
+        print(vermelho + "Encerrando..." + reset)
+        sys.exit(1)
 
 
 def show_banner() -> str:
@@ -21,62 +45,73 @@ def show_banner() -> str:
     print(amarelo + bold + "-"*30 + reset)
     print(amarelo + bold + "GitHub: https://github.com/sa1n255" + reset)
     print(amarelo + bold + "Coded by Sa1n" + reset)
-
     print("")
+
+
+def confirm():
+    confirmar = input("Você tem certeza que deseja prosseguir mesmo assim? [Y/n]: ").lower()
+    if 'y' in confirmar or 's' in confirmar:
+        print(verde + 'Prosseguindo' + reset)
+        return True
+    else:
+        print("Abortando")
+        return False
 
 
 def password_generator(password_lenght: int = 8) -> str:
     upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     reduced_punctuation = '&$%#@'
     all_options = all_letters + all_numbers + reduced_punctuation
+
     try:
         while True:
 
-            # A Senha do Usuario sempre deve começar com letra Maiuscula
-            # A Senha do Usuario sempre deve conter números
-            # A Senha do Usuario sempre deve conter pontuações
-            # A Senha do Usuário nunca deve ter mais de dois caracteres repetidos
+            # 1- A Senha do Usuario sempre deve começar com letra Maiuscula
+            # 2- A Senha do Usuario sempre deve conter números
+            # 3- A Senha do Usuario sempre deve conter pontuações
+            # 4- A Senha do Usuário nunca deve ter mais de dois caracteres repetidos
 
             user_password = ""
             user_password += choice(upper_letters)
 
-            for i in range(password_lenght-1):
-                user_password += choice(all_options)
-            # Checa se a senha tem Números
-            for i in all_numbers:
-                if i not in user_password:
-                    password_has_number = False
+            # Gera a senha
+            for i in range(password_lenght-1): user_password += choice(all_options)
+
+            # Checa se a senha gerada tem Números
+            for i in all_numbers: 
+                if i not in user_password: password_has_number = False
                 else:
                     password_has_number = True
                     break
-            # Checa se a senha tem Letras
+
+            # Checa se a senha gerada tem Letras
             for i in all_letters:
-                if i not in user_password:
-                    password_has_letters = False
+                if i not in user_password: password_has_letters = False
                 else:
                     password_has_letters = True
                     break
-            # Checa se a senha tem Pontuações
+
+            # Checa se a senha gerada tem Pontuações
             for i in reduced_punctuation:
-                if i not in user_password:
-                    password_has_punc = False
+                if i not in user_password: password_has_punc = False
                 else:
                     password_has_punc = True
                     break
-            # Checa se a senha tem muitos caracteres repetidos
+
+            # Checa se a senha gerada tem mais de 2 caracteres repetidos
             for i in user_password:
                 if user_password.count(i) > 2:
                     too_many_characters = True
                     break
-                else:
-                    too_many_characters = False
-            if (password_has_number == True and password_has_punc == True and password_has_letters == True and too_many_characters == False) == True:
+                else: too_many_characters = False
+        
+            phn, php, phl, tmc = password_has_number, password_has_punc, password_has_letters, too_many_characters
+            # Checa se a senha gerada atende os 4 requisitos
+            if (phn == True and php == True and phl == True and tmc == False) == True:
                 return user_password
-            else:
-                pass
 
-    except KeyboardInterrupt as ki:
+    except KeyboardInterrupt as ki: 
         exceptions.keyboard_interrupt(ki)
 
-    except Exception as error:
+    except Exception as error: 
         exceptions.exception(error)
--- main.py
+++ main.py
@@ -2,22 +2,36 @@ from functions.functions import *
 from functions import exceptions
 from functions.colors import *
 
-def main():
+import sys
+
+def main(): 
     show_banner()
 
-    try:
+    try: 
         user_password_length = int(input("Tamanho da senha: (Recomendado: 8 até 50)\n>>> "))
 
-    except KeyboardInterrupt as ki:
+        while user_password_length <= 2:
+            print(vermelho + "Só é possivel gerar senhas com no minimo 3 indices de tamanho" + reset)
+            user_password_length = int(input("Tamanho da senha: (Recomendado: 8 até 50)\n>>> "))
+
+        if user_password_length <= 7: 
+            print(amarelo + "Aviso: Sua senha é menor que 8 indices." + reset)
+            c = confirm()
+            if not c:
+                print(vermelho + "Encerrando..." + reset)
+                sys.exit(1)
+
+    except KeyboardInterrupt as ki: 
         exceptions.keyboard_interrupt(ki)
 
-    except Exception as error:
+    except Exception as error: 
         exceptions.exception(error)
     
-    else:
+    else: 
         print("Gerando senha...")
 
     print(f"Senha gerada: {bold + password_generator(user_password_length) + reset}")
 
 if __name__ == '__main__':
+    version_check()
     main()
